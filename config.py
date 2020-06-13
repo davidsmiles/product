8d0543ab2481d55adeab7cb2ec932563e2532dc2
davidsmiles <ugberodavid@gmail.com>
2020-06-13 03:59:08 +0100

    Refactor for docker mongodb database

--- config.py
+++ config.py
@@ -4,9 +4,6 @@ import os
 class BaseConfig():
     ENV = 'development'
     DEBUG = True
-    MONGODB_SETTINGS = {
-        'host': os.environ.get("PROD_DATABASE_URL", "mongodb://localhost:27017/webshop")
-    }
     PROPAGATE_EXCEPTIONS = True
     JWT_SECRET_KEY = 'microservice'
     SEND_FILE_MAX_AGE_DEFAULT = 0
@@ -23,6 +20,9 @@ class TestConfig(BaseConfig):
 
 class DevelopmentConfig(BaseConfig):
     DEBUG = True
+    MONGODB_SETTINGS = {
+        'host': os.environ.get("PROD_DATABASE_URL", "mongodb://david:secret@database/webshop")
+    }
 
 
 class ProductionConfig(BaseConfig):
